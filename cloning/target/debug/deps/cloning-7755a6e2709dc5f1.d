9672aa115541c189170b7b12ab0f2df04b98954f
Tony <tony@Tonys-MacBook-Air.local>
2024-04-25 01:03:34 +0100

    create app mod

--- cloning/target/debug/deps/cloning-7755a6e2709dc5f1.d
+++ cloning/target/debug/deps/cloning-7755a6e2709dc5f1.d
@@ -1,5 +1,6 @@
-/Users/tony/Documents/E/Projects/Rust_DNA/Cloning/cloning/target/debug/deps/libcloning-7755a6e2709dc5f1.rmeta: src/main.rs
+/Users/tony/Documents/E/Projects/Rust_DNA/Cloning/cloning/target/debug/deps/libcloning-7755a6e2709dc5f1.rmeta: src/main.rs src/app.rs
 
-/Users/tony/Documents/E/Projects/Rust_DNA/Cloning/cloning/target/debug/deps/cloning-7755a6e2709dc5f1.d: src/main.rs
+/Users/tony/Documents/E/Projects/Rust_DNA/Cloning/cloning/target/debug/deps/cloning-7755a6e2709dc5f1.d: src/main.rs src/app.rs
 
 src/main.rs:
+src/app.rs:
